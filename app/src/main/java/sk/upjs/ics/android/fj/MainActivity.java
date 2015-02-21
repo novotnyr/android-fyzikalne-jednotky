b7ba7df3bd17a32570826e0f7120461afd70a00c
novotnyr <robert.novotny@upjs.sk>
2015-02-21 20:33:07 +0100

    Initial Import

---
+++ app/src/main/java/sk/upjs/ics/android/fj/MainActivity.java
@@ -0,0 +1,60 @@
+package sk.upjs.ics.android.fj;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+    private TextView lblFahrenheit;
+
+    private EditText txtCelsius;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lblFahrenheit = (TextView) findViewById(R.id.lblFahrenheit);
+
+        txtCelsius = (EditText) findViewById(R.id.txtCelsius);
+        txtCelsius.addTextChangedListener(new CelsiusTextWatcher());
+    }
+
+    private class CelsiusTextWatcher implements TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            updateFahrenheit();
+        }
+    }
+
+    private void updateFahrenheit() {
+        try {
+            double celsius = Double.parseDouble(txtCelsius.getText().toString());
+            double fahrenheit = ((9 / 5) * celsius) + 32;
+            lblFahrenheit.setText(Double.toString(fahrenheit));
+        } catch (NumberFormatException e) {
+            Toast
+                    .makeText(this, "Please enter a number", Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+}
